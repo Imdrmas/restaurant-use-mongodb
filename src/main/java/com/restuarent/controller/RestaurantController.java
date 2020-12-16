987ff34e35e8a4914438de3eb455f04f7c98ffcc
asmail11 <imdrmas@gmail.com>
2020-12-16 13:36:35 +0100

    second commit

--- src/main/java/com/restuarent/controller/RestaurantController.java
+++ src/main/java/com/restuarent/controller/RestaurantController.java
@@ -1,14 +1,13 @@
 package com.restuarent.controller;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.restuarent.dao.RestaurantDao;
 import com.restuarent.model.Restaurant;
 
